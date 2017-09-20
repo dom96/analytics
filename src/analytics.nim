eca95e17b0c33b981477644cd39ae1ad5811198a
Dominik Picheta <dominikpicheta@gmail.com>
2017-09-20 14:15:55 +0100

    Implement reportException proc.

--- src/analytics.nim
+++ src/analytics.nim
@@ -17,6 +17,9 @@ type
     an: string ## Application name
     av: string ## Application version
 
+const
+  collectUrl = "https://www.google-analytics.com/collect"
+
 proc newAnalytics*(trackingID, clientID, appName, appVer: string,
                    userAgent = ""): Analytics =
   ## Creates a new analytics reporting object.
@@ -42,9 +45,16 @@ proc newAnalytics*(trackingID, clientID, appName, appVer: string,
     av: appVer
   )
 
+proc createCommonPayload(this: Analytics): string =
+  var payload = "v=1&aip=1&t=event"
+  payload.add("&tid=" & encodeUrl(this.tid))
+  payload.add("&cid=" & encodeUrl(this.cid))
+  payload.add("&an=" & encodeUrl(this.an))
+  payload.add("&av=" & encodeUrl(this.av))
+  return payload
+
 proc reportEvent*(this: Analytics, category, action, label: string = "",
                   value: Option[int] = none(int)) =
-  var uri = parseUri("https://www.google-analytics.com/collect")
 
   if category.len == 0:
     raise newException(ValueError, "Category cannot be empty.")
@@ -52,11 +62,7 @@ proc reportEvent*(this: Analytics, category, action, label: string = "",
   if action.len == 0:
     raise newException(ValueError, "Action cannot be empty.")
 
-  var payload = "v=1&aip=1&t=event"
-  payload.add("&tid=" & encodeUrl(this.tid))
-  payload.add("&cid=" & encodeUrl(this.cid))
-  payload.add("&an=" & encodeUrl(this.an))
-  payload.add("&av=" & encodeUrl(this.av))
+  var payload = createCommonPayload(this)
 
   payload.add("&ec=" & encodeUrl(category))
   payload.add("&ea=" & encodeUrl(action))
@@ -65,7 +71,20 @@ proc reportEvent*(this: Analytics, category, action, label: string = "",
   if value.isSome:
     payload.add("&ev=" & $value.get())
 
-  discard this.client.postContent($uri, body=payload)
+  discard this.client.postContent(collectUrl, body=payload)
+
+proc reportException*(this: Analytics, description: string, isFatal=true) =
+  ## Reports an exception to analytics.
+  ##
+  ## To get this data in analytics, see:
+  ## https://stackoverflow.com/a/21718577/492186
+
+  var payload = createCommonPayload(this)
+
+  payload.add("&exd=" & encodeUrl(description))
+  payload.add("&exf=" & $(if isFatal: 1 else: 0))
+
+  discard this.client.postContent(collectUrl, body=payload)
 
 proc genClientID*(): string =
   return $genUUID()
@@ -74,4 +93,7 @@ when isMainModule:
   let tid = "UA-105812497-2"
   let cid = genClientID()
   let analytics = newAnalytics(tid, cid, "AnalyticsTester", "0.1")
-  analytics.reportEvent("AnalyticsTest", "nim c analytics")
+  analytics.reportEvent("InstallSuccess", "nim c analytics", "", some(4567))
+  analytics.reportEvent("InstallFailure", "nim c analytics",
+                        "Error: .... hello lorem ipsum dolor sit amet",
+                        some(1234))
